34633a03af4fbf2abbc9a4581a9f4407b154574b
Neil Ellis <neil@cazcade.com>
2014-06-18 11:20:35 +0100

    Fixes

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /root
-export HOME=/var/easydeploy/share
+export HOME=/root
 /root/logstash-1.4.1/bin/logstash agent -f /root/logstash.conf -- web  &
 while :
 do
-     curator delete --disk-space 100
+    curator delete --disk-space 100
     sleep 1200
 done
